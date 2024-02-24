b960f9ad716e7d011d8f224ccb88e5c8298b31a9
chaitu-ycr <chaitu.ycr@gmail.com>
2024-02-24 14:46:06 +0530

    added dependencies for diag desc parsing

---
+++ src/py_uds_lib_utils/diag_parser.py
@@ -0,0 +1,12 @@
+class CddParser:
+    """CDD (CANdelaStudio Diagnostic Description).
+    """
+    def __init__(self) -> None:
+        pass
+
+
+class PdxParser:
+    """ODX is an ISO (ISO-22901) and ASAM (ASAM MCD2D) standardized XML-based file format.
+    """
+    def __init__(self) -> None:
+        pass
